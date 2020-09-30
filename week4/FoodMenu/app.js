44bf5a3ea79bcc196842c61d34c7c14671fe1c9c
ChapmanChappelle <chapman.chappelle@gmail.com>
2020-09-30 17:09:29 -0400

    food menu pt 1

---
+++ week4/FoodMenu/app.js
@@ -0,0 +1,42 @@
+let dishesUL = document.getElementById("dishesUL")
+let appBtn = document.getElementById("appBtn")
+let entreeBtn = document.getElementById("entreeBtn")
+let dessertBtn = document.getElementById("dessertBtn")
+
+//displaying food function
+displayDishes = function (dish) {
+    for (let i = 0; i < dishes.length; i++) {
+        let dish = dishes[i]
+
+        dishItem = `
+            <li>
+                <b>${dish.title}</b>
+                <p>${dish.description}</p>
+                <p>${dish.price}</p>
+                <img src="${dish.imageURL}"></img>
+            </li>`
+    dishesUL.insertAdjacentHTML("afterend", dishItem)
+    }
+}
+// clearing list function
+emptyList = function () {
+        
+  var ul = document.querySelector("#dishesDiv")
+  var listLength = ul.children.length
+  for (i = 0; i < listLength; i++) {
+    ul.removeChild(ul.children[0])
+  }
+}
+displayDishes()
+
+// button filters
+appBtn.addEventListener("click", function () {
+    emptyList()
+    displayDishes()
+})
+entreeBtn.addEventListener("click", function () {
+    emptyList()
+})
+dessertBtn.addEventListener("click", function () {
+    emptyList()
+})
