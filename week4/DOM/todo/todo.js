c8613a9e92c8a7570d73e85b5b6bb0aa02c46c37
ChapmanChappelle <chapman.chappelle@gmail.com>
2020-09-30 10:34:11 -0400

    cleaned todo

--- week4/DOM/todo/todo.js
+++ week4/DOM/todo/todo.js
@@ -1,30 +1,36 @@
+// giving elements pointer objects
 let taskNameTxtBox = document.getElementById("taskNameTxtBox")
 let saveBtn = document.getElementById("saveBtn")
 let tasksUL = document.getElementById("taskUL")
 let completedUL = document.getElementById("completedUL")
 
+// when button is clicked
 saveBtn.addEventListener("click", function () {
-  //create a li element
+  // create a li element
   let li = document.createElement("li")
-  //create a label
+  
+  // create a label
   let lbl = document.createElement("label")
+
   // adding checkbox
-  // also added checkbox
+  // also added checkbox listener
   let checkBox = document.createElement("input")
   checkBox.type = "checkbox"
   checkBox.id = "check"
   checkBox.checked = false
   checkBox.addEventListener("click", function () {
-    completedUL.appendChild(this.parentElement)
+    completedUL.appendChild(this.parentElement) // takes the li out of tasksUL and into completedUL
   })
+
   // adding button
   let removeBtn = document.createElement("button")
   removeBtn.id = "removeBtn"
   removeBtn.innerHTML = "Remove"
   removeBtn.addEventListener("click", function () {
-    let parent = this.parentElement.parentElement
+    let parent = this.parentElement.parentElement // have to use grandparent here to move the item back to the first list
     parent.removeChild(this.parentElement)
   })
+
   // change the inner html to reflect the value from textbox
   taskName = taskNameTxtBox.value
   lbl.innerHTML = taskName
