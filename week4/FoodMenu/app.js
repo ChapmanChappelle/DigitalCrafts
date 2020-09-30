a3fa03ee13addc0353419792750e3768575fb2d6
ChapmanChappelle <chapman.chappelle@gmail.com>
2020-09-30 18:35:37 -0400

    worked some more on food menu

--- week4/FoodMenu/app.js
+++ week4/FoodMenu/app.js
@@ -1,43 +1,120 @@
-let dishesUL = document.getElementById("dishesUL")
+/*
+let dishesListDiv = document.getElementById("dishesListDiv")
+let courseTitle = document.getElementById("courseTitles")
+let allBtn = document.getElementById("allBtn")
 let appBtn = document.getElementById("appBtn")
 let entreeBtn = document.getElementById("entreeBtn")
 let dessertBtn = document.getElementById("dessertBtn")
 
-//displaying food function
-displayDishes = function (course) {
-    for (let i = 0; i < dishes.length; i++) {
-        let dish = dishes[i]
-
-        dishItem = `
-            <li>
-                <b>${dish.title}</b>
-                <p>${dish.description}</p>
-                <p>${dish.price}</p>
-                <img src="${dish.imageURL}"></img>
-            </li>`
-    dishesUL.insertAdjacentHTML("afterend", dishItem)
-    }
-}
-
-// clearing list function
-emptyList = function () {
-        
-  var ul = document.querySelector("#dishesDiv")
-  var listLength = ul.children.length
-  for (i = 0; i < listLength; i++) {
-    ul.removeChild(ul.children[0])
+// if statement for course type
+let getDishes = (course) => {
+  if (course === "Full Menu") {
+    return dishes
+  } else {
+    return dishes.filter((dish) => dish.course === course)
   }
 }
-displayDishes()
+//displaying food function
+writeDishes = (course) => {
+  let displayDishes = getDishes(course)
+
+  let mapDishes = displayDishes.map((dish) => {
+    let dishDiv = `<div class="courseDiv">
+                        <div class="courseImg">
+                            <img src="${dish.imageURL}" />
+                        </div>
+                        <div class="courseTitleDescrip">
+                            <h2>${dish.title}</h2>
+                            <p>${dish.description}</p>
+                        </div>
+                        <div class="coursePrice">
+                            <p>\$${dish.price}</p>
+                        </div>
+                    </div>`
+    return dishDiv
+  })
+
+  // update title of course
+  courseTitle.innerHTML = course
+  // Write courses
+  dishesListDiv.innerHTML = mapDishes.join("")
+}
+
+writeDishes("Full Menu")
 
 // button filters
 appBtn.addEventListener("click", function () {
-    emptyList()
-    displayDishes()
+  writeDishes("Starters")
 })
 entreeBtn.addEventListener("click", function () {
-    emptyList()
+  writeDishes("Entrees")
 })
 dessertBtn.addEventListener("click", function () {
-    emptyList()
+  writeDishes("Desserts")
+})
+*/
+
+
+let dishesListDiv = document.getElementById("dishesListDiv")
+let courseTitle = document.getElementById("courseTitle")
+
+// buttons
+let fullButton = document.getElementById("fullMenuButton")
+let starterButton = document.getElementById("fullStartersButton")
+let entreeButton = document.getElementById("fullEntreesButton")
+let dessertButton = document.getElementById("fullDessertsButton")
+
+let getDishes = (course) => {
+  if (course === "Full Menu") {
+    return dishes
+  } else {
+    return dishes.filter((dish) => dish.course === course)
+  }
+}
+
+let writeCourseToDOM = (course) => {
+  let displayDishes = getDishes(course)
+
+  let mapDishes = displayDishes.map((dish) => {
+    let dishDiv = `<div class="courseDiv">
+                        <div class="courseImg">
+                            <img src="${dish.imageURL}" />
+                        </div>
+                        <div class="courseTitleDescrip">
+                            <h2>${dish.title}</h2>
+                            <p>${dish.description}</p>
+                        </div>
+                        <div class="coursePrice">
+                            <p>\$${dish.price}</p>
+                        </div>
+                    </div>`
+    return dishDiv
+  })
+
+  // Update the course title
+  courseTitle.innerHTML = course
+  // Write courses down
+  dishesListDiv.innerHTML = mapDishes.join("")
+}
+
+writeCourseToDOM("Full Menu")
+
+// (Full Button)
+fullButton.addEventListener("click", function () {
+  writeCourseToDOM("Full Menu")
+})
+
+// (Starter Button)
+starterButton.addEventListener("click", function () {
+  writeCourseToDOM("Starters")
+})
+
+// (Entree Button)
+entreeButton.addEventListener("click", function () {
+  writeCourseToDOM("Entrees")
+})
+
+// (Dessert Button)
+dessertButton.addEventListener("click", function () {
+  writeCourseToDOM("Desserts")
 })
