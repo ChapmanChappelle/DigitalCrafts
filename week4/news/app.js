247d4ac8fdc7636b06dfc64b2b1c57bba24b7b8d
ChapmanChappelle <chapman.chappelle@gmail.com>
2020-10-01 16:55:13 -0400

    tried (and failed) to do the hard part

--- week4/news/app.js
+++ week4/news/app.js
@@ -1,48 +1,59 @@
-/*
-//DISPLAY MOVIES ON SCREEN
-let moviesListUL = document.getElementById("moviesList")
+//getting divs from index.html
+let articleBox = document.getElementById("articleBox")
+let sourcesBox = document.getElementById("sourcesBox")
+let sourceTxt = document.getElementById("sourceTxt")
+let searchBtn = document.getElementById("searchBtn")
 
-let movieItems = movies.map((movie) => { 
-    return `
-        <div>
-            <b>${movie.Title}</b>
-            <img src="${movie.Poster}"></p>
-            <p>${movie.Year}</p>
-        </div>}`
-})
-
-moviesListUL.insertAdjacentHTML('beforeend', movieItems.join(""))
- */
-
-let articleBox = document.getElementById("news")
-
+//checking for null article properties
+let checkNull = (item) => {
+  if (item == null) {
+    item = " "
+    return item
+  } else {
+    return item
+  }
+}
+// mapping article
 let articleItem = news.articles.map((article) => {
-
-    if (article.author == null) {
-        article.author = " "
-    } if (article.title == null) {
-        article.title = " "
-    } if (article.description == null) {
-        article.description = " "
-    } if (article.url == null) {
-        article.url = " "
-    } if (article.urlToImage == null) {
-        article.urlToImage = " "
-    } if (article.publishedAt == null) {
-        article.publishedAt = " "
-    }
-
-
-    let articleInfo = ` <div id="article">
-                            <p>${article.author}</p>
-                            <b class="title">${article.title}</b>
-                            <p>${article.description}</p>
-                            <a href"${article.url}"></a>
-                            <img class="imgs" src="${article.urlToImage}"></img>
-                            <p>${article.publishedAt}</p>
+  // template literals
+  let articleInfo = ` <div id="article">
+                            <p>${checkNull(article.author)}</p>
+                            <b class="title">${checkNull(article.title)}</b>
+                            <p>${checkNull(article.description)}</p>
+                            <a href="${checkNull(article.url)}">See Article</a>
+                            <img class="imgs" src="${checkNull(
+                              article.urlToImage
+                            )}"></img>
+                            <p>${checkNull(article.publishedAt)}</p>
                         </div>`
-
+  //return article item
   return articleInfo
 })
-console.log(articleItem)
-articleBox.innerHTML = articleItem.join(" ")
+// writing items to articleBox
+articleBox.innerHTML = articleItem.join("")
+
+// mapping sources
+let sourcesItem = sources.sources.map((source) => {
+  //template literals
+  let sourceInfo = ` <div id="source">
+                            <p>${source.id}</p>
+                            <p>${source.name}</p>
+                            <p>${source.description}</p>
+                            <a href="${source.url}">See Source</a>
+                            <p>${source.category}</p>
+                            <p>${source.language}</p>
+                            <p>${source.country}</p>
+                        </div>`
+  // returning source item
+  return sourceInfo
+})
+// writing items to sourcesBox
+//sourcesBox.innerHTML = sourcesItem.join("")
+
+
+searchBtn.addEventListener("click", function () {
+
+    let input = sourceTxt.value
+
+    sourcesBox.innerHTML = sourcesItem
+})
