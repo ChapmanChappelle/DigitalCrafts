586e39aab844334ee983906ae1be0eebd7cc2742
ChapmanChappelle <chapman.chappelle@gmail.com>
2020-10-01 11:45:45 -0400

    did the timer activity

--- week4/movies/js/app.js
+++ week4/movies/js/app.js
@@ -1,3 +1,5 @@
+/* 
+//DISPLAY MOVIES ON SCREEN
 let moviesListUL = document.getElementById("moviesList")
 
 let movieItems = movies.map((movie) => { 
@@ -10,3 +12,21 @@ let movieItems = movies.map((movie) => {
 })
 
 moviesListUL.insertAdjacentHTML('beforeend', movieItems.join(""))
+ */
+
+//TIMER PROGRAM
+let userTxt = document.getElementById("userTxt")
+let startBtn = document.getElementById("startBtn")
+let timerH1 = document.getElementById("timerH1")
+
+startBtn.addEventListener("click", function () {
+  let time = userTxt.value
+  let intervalID = window.setInterval(() => {
+    time--
+    timerH1.innerHTML = time
+    if (time == 0) {
+      window.clearInterval(intervalID)
+      timerH1.innerHTML = "DING DING DING"
+    }
+  }, 1000)
+})
