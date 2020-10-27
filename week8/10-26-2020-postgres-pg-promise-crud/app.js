116546a0b1c89bf883ffa2b2e7a6751c4a462618
ChapmanChappelle <chapman.chappelle@gmail.com>
2020-10-27 14:37:07 -0400

    updated azam's example

--- week8/10-26-2020-postgres-pg-promise-crud/app.js
+++ week8/10-26-2020-postgres-pg-promise-crud/app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express() 
 // initialize pg-promise library
 const pgp = require('pg-promise')()
+var bcrypt = require('bcryptjs');
 
-const connectionString = 'postgres://localhost:5432/chapmanchappelle'
+const connectionString = 'postgres://localhost:5432/mydatabase'
 
 // initialize pg promise by using a connection string 
 // pgp(...) returns an object which contains functions to interact with the database 
@@ -34,6 +35,43 @@ app.get('/',(req,res) => {
     //console.log("AT THE END OF FUNCTION")
 })
 
+
+app.get('/dashboard',async (req,res) => {
+
+    let result = await db.any('SELECT users.user_id,first_name, last_name, age, street, city, state FROM users JOIN addresses ON users.user_id = addresses.user_id')
+
+    let users = formatUsersAndAddressesForDisplay(result)
+    
+    console.log(users)
+
+    res.render('dashboard', {usersAddresses: users})
+})
+
+function formatUsersAndAddressesForDisplay(list) {
+
+    let users = [] 
+
+    list.forEach((item) => {
+        if(users.length == 0) {
+            let user = {userId: item.user_id,firstName: item.first_name,
+                 lastName: item.last_name,addresses: [{city: item.city, street: item.street}]}
+            users.push(user)
+        } else {
+            let user = users.find(user => user.userId == item.user_id)
+            if(user) {
+                user.addresses.push({city: item.city, street: item.street})
+            } else {
+                let user = {userId: item.user_id,firstName: item.first_name,
+                    lastName: item.last_name,addresses: [{city: item.city, street: item.street}]}
+               users.push(user)
+            }
+        }
+    })
+
+    return users 
+
+}
+
 app.post('/delete-user',(req,res) => {
 
     const userId = req.body.userId 
@@ -43,6 +81,22 @@ app.post('/delete-user',(req,res) => {
             res.redirect('/')
         })
 
+})
+
+
+app.post('/register',(req,res) => {
+
+    const username = req.body.username 
+    const password = req.body.password 
+
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(password, salt, function(err, hash) {
+            db.none('INSERT INTO registrations(username, password) VALUES($1,$2)',[username, hash])
+            .then(() => {
+                res.redirect('/dashboard')
+            })
+        })
+    })
 })
 
 app.post('/create-user',(req,res) => {
