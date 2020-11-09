a33040fb78d9ab18cefcb7bd87c9688e9db756b7
ChapmanChappelle <chapman.chappelle@gmail.com>
2020-11-09 15:25:31 -0500

    finished highoncoding

--- week11/highOnCoding/src/Articles.js
+++ week11/highOnCoding/src/Articles.js
@@ -18,15 +18,14 @@ class Articles extends Component {
             </div>
             <h3 class="title">Introduction to Swift</h3>
             <p class="info">
-               Last month Apple released the anticipated WatchKit Framework for
-               developers in the form of iOS 8.2 beta SDK release. The WatchKit
-               framework enables the developers to create Apple Watch
-               applications. In this article we are going to focus on the basics
-               of getting started wuth the WatchKit framework and developing
-               apps for the Apple Watch.
+               Swift is a modern programming language developed by Apple to
+               create the next generation of iOS and OSX applications. Swift is
+               a fairly new language and is still in development but it clearly
+               reflects the intentions and the future direction. This article
+               will resolve around the basic concepts in the Swift language and
+               how you can get started
             </p>
             <p class="bar">15 comments 45 likes</p>
-
          </div>
       )
    }
